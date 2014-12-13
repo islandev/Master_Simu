ad11bf209ba8c79bb2ace083f6ab32d1e7e2094f
islandev <ben.wangdev@gmail.com>
2014-12-14 05:12:14 +0800

    some change
    some change  on code to calculate the v  k  d

--- mlc.m
+++ mlc.m
@@ -77,8 +77,14 @@ val_left=c2^3/c3^2;
 % eq=''val_left=psi(1,x).^3/psi(2,x).^2;
 % res=solve(eq);
 % 
-f=@(x)norm(psi(1,x).^3/psi(2,x).^2-val_left).^2;
-[x,err]=fminsearch(f,30);
+f=@(k)norm(psi(1,k).^3/psi(2,k).^2-val_left).^2;
+[k,err]=fminsearch(f,30);
+
+v=(psi(1,k)/c2).^0.5;
+
+d=exp(c1-psi(k)/v);
+
+
 
 
 
--- plotsimu.m
+++ plotsimu.m
@@ -2,5 +2,4 @@ clc
 x=1:2;
 y=psi(1,x).^3/psi(2,x).^2;
 plot(x,y,'r'); 
-
- 
+plot(plot::Function2d(psi(1,x).^3/psi(2,x).^2),x=-1..1,XMesh=1000)):
