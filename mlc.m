399dab1d70463aaca2aa832dfa6b76eebb95ac82
islandev <ben.wangdev@gmail.com>
2014-12-19 00:31:49 +0800

    code  update
    some update  on  k  and  d

--- mlc.m
+++ mlc.m
@@ -27,20 +27,20 @@ fIn = fopen([filePath 'C13_imag.bin'],'r');
 data(:,:,8) = fread(fIn,[col,row],'float').';   fclose(fIn);
 fIn = fopen([filePath 'C23_imag.bin'],'r');
 data(:,:,9) = fread(fIn,[col,row],'float').';   fclose(fIn); 
-
+data_1=data(:,:,9)
 
 c_data(:,:,1)=data(:,:,1); %c11
 c_data(:,:,2)=data(:,:,2); %c22
 c_data(:,:,3)=data(:,:,3); %c33
-
-% c_data(:,:,4)=sqrt(data(:,:,4).^2+data(:,:,7).^2); %c12
-% c_data(:,:,4)=sqrt(data(:,:,5).^2+data(:,:,8).^2); %c13
-% c_data(:,:,4)=sqrt(data(:,:,6).^2+data(:,:,9).^2); %c23
-% c_data(:,:,4)=sqrt(data(:,:,4).^2+data(:,:,7).^2); %c21
-% c_data(:,:,4)=sqrt(data(:,:,5).^2+data(:,:,8).^2); %c31
-% c_data(:,:,4)=sqrt(data(:,:,6).^2+data(:,:,9).^2); %c32
-
-
+% 
+% % c_data(:,:,4)=sqrt(data(:,:,4).^2+data(:,:,7).^2); %c12
+% % c_data(:,:,4)=sqrt(data(:,:,5).^2+data(:,:,8).^2); %c13
+% % c_data(:,:,4)=sqrt(data(:,:,6).^2+data(:,:,9).^2); %c23
+% % c_data(:,:,4)=sqrt(data(:,:,4).^2+data(:,:,7).^2); %c21
+% % c_data(:,:,4)=sqrt(data(:,:,5).^2+data(:,:,8).^2); %c31
+% % c_data(:,:,4)=sqrt(data(:,:,6).^2+data(:,:,9).^2); %c32
+% 
+% 
 c_data(:,:,4)=data(:,:,4)+1i*data(:,:,7); %c12
 c_data(:,:,5)=data(:,:,5)+1i*data(:,:,8); %c13
 c_data(:,:,6)=data(:,:,6)+1i*data(:,:,9); %c23
@@ -54,7 +54,19 @@ for i=1:9
     m1=log(det(c_data(:,:,i)))+m1;   
     m2=log(det(c_data(:,:,i)))^2+m2;
     m3=log(det(c_data(:,:,i)))^3+m3;
+    
 end
+
+% for i=1:9
+%     m1=log(det(data(:,:,i)))+m1;   
+%     m2=log(det(data(:,:,i)))^2+m2;
+%     m3=log(det(data(:,:,i)))^3+m3;
+%     
+% end
+
+
+
+
  m1=m1/9;
  m2=m2/9;
  m3=m3/9;
