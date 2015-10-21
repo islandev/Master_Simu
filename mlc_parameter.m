120db291d76013f1dc308d959f650dc4314eafc6
islandev <ben.wangdev@gmail.com>
2015-10-22 01:16:32 +0800

    Update mlc_parameter.m
    merge code

--- mlc_parameter.m
+++ mlc_parameter.m
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-clear all;clc;
-
-filePath = 'C:\Users\Administrator\Desktop\data_polsar\AIRSAR_SanFrancisco\sea\C3\';
-
-row=127;
-col=127 ;
-%调用函数 求基于矩阵的对数累积量的参数
-[k,sigma,v,covhh,covhv,covvv]=ParameterEstimation(filePath,row,col); %单通道
-%[k,sigma,v,covhh]=ParameterEstimationhh(filePath,row,col); 
-
-
-
-%[k,sigma,v]= mlcparameter( filePath,row,col ) %异质纹理
-=======
 clc;
 
 clear all;
@@ -29,7 +14,6 @@ col=127 ;
 
 
 
->>>>>>> c6aeb1a21731875e96727aeb0dcad5739ec869c9
 
     
 
